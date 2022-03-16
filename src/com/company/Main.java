d82dbf6974216e0dc839524754fd918ce1d4e126
Muhammed <muhammed.almazbekovich@gmail.com>
2022-03-16 09:46:05 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,9 @@
 package com.company;
 
-import java.sql.Array;
-
 public class Main {
 
     public static void main(String[] args) {
 
-
-        Class myObj = new Class(12 , "WORD", new int[]{1,2,4});
-
-        System.out.println(myObj.getNumber());
-        System.out.println(myObj.getWord());
-        System.out.println(myObj.getArrNumber());
-
-
-
-
-
-
+       Class myObj = new Class(7, "Seven", new int[]{7, 77, 777});
     }
 }
